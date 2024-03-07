3529569af8268e40be51676a3f90870de96841fc
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-03-07 17:48:34 -0500

    initial commit

---
+++ src/main/java/web/mates/arriendatufinca/ArriendatufincaApplication.java
@@ -0,0 +1,13 @@
+package web.mates.arriendatufinca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArriendatufincaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArriendatufincaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/web/mates/arriendatufinca/ArriendatufincaApplicationTests.java
@@ -0,0 +1,13 @@
+package web.mates.arriendatufinca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArriendatufincaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
