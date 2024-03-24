8448648a605339157dbcbae0c6ee4d080000f8fe
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-03-24 18:46:58 -0500

    feat: delete user's properties when deleting user

--- src/main/java/web/mates/arriendatufinca/service/PropertyService.java
+++ src/main/java/web/mates/arriendatufinca/service/PropertyService.java
@@ -13,67 +13,74 @@ import web.mates.arriendatufinca.model.Property;
 import web.mates.arriendatufinca.model.User;
 import web.mates.arriendatufinca.repository.PropertyRepository;
 
+import javax.swing.text.html.Option;
+
 @Service
 public class PropertyService {
 
-  private final PropertyRepository propertyRepository;
-  private final UserService userService;
-  private final ModelMapper modelMapper;
+    private final PropertyRepository propertyRepository;
+    private final UserService userService;
+    private final ModelMapper modelMapper;
 
-  PropertyService(PropertyRepository propertyRepository, UserService userService, ModelMapper modelMapper) {
-    this.propertyRepository = propertyRepository;
-    this.userService = userService;
-    this.modelMapper = modelMapper;
-  }
-
-  public List<PropertyDTO> getAllProperties() {
-    Iterable<Property> properties = propertyRepository.findAll();
-    List<PropertyDTO> propertiesDTO = new ArrayList<>();
-
-    for (Property property : properties) {
-      propertiesDTO.add(modelMapper.map(property, PropertyDTO.class));
+    PropertyService(PropertyRepository propertyRepository, UserService userService, ModelMapper modelMapper) {
+        this.propertyRepository = propertyRepository;
+        this.userService = userService;
+        this.modelMapper = modelMapper;
     }
-    return propertiesDTO;
-  }
 
-  public PropertyDTO getPropertyById(@NonNull UUID id) {
-    Optional<Property> property = propertyRepository.findById(id);
-    if (property.isPresent())
-      return modelMapper.map(property, PropertyDTO.class);
-    return null;
-  }
+    public List<PropertyDTO> getAllProperties() {
+        Iterable<Property> properties = propertyRepository.findAll();
+        List<PropertyDTO> propertiesDTO = new ArrayList<>();
 
-  public PropertyDTO newProperty(@NonNull PropertyDTO property) {
-    Property newProperty = modelMapper.map(property, Property.class);
-
-    newProperty.setOwner(
-        modelMapper.map(
-            userService.getUserById(property.getOwnerID()),
-            User.class));
-
-    propertyRepository.save(newProperty);
-    return getPropertyById(newProperty.getId());
-  }
-
-  public PropertyDTO updateProperty(@NonNull UUID id, @NonNull PropertyDTO newProperty) {
-    Property property = propertyRepository.findById(id).get();
-    property.setName(newProperty.getName());
-    property.setDescription(newProperty.getDescription());
-    property.setBathrooms(newProperty.getBathrooms());
-    property.setRooms(newProperty.getRooms());
-    property.setPricePerNight(newProperty.getPricePerNight());
-    property.setBbq(newProperty.isBbq());
-    property.setPetFriendly(newProperty.isPetFriendly());
-    property.setPool(newProperty.isPool());
-    propertyRepository.save(property);
-    return getPropertyById(property.getId());
-  }
-
-  public Boolean deleteProperty(@NonNull UUID id) {
-    if (propertyRepository.existsById(id)) {
-      propertyRepository.deleteById(id);
-      return true;
+        for (Property property : properties) {
+            propertiesDTO.add(modelMapper.map(property, PropertyDTO.class));
+        }
+        return propertiesDTO;
     }
-    return false;
-  }
+
+    public PropertyDTO getPropertyById(@NonNull UUID id) {
+        Optional<Property> property = propertyRepository.findById(id);
+        if (property.isPresent())
+            return modelMapper.map(property, PropertyDTO.class);
+        return null;
+    }
+
+    public PropertyDTO newProperty(@NonNull PropertyDTO property) {
+        Property newProperty = modelMapper.map(property, Property.class);
+
+        newProperty.setOwner(
+                modelMapper.map(
+                        userService.getUserById(property.getOwnerID()),
+                        User.class));
+
+        propertyRepository.save(newProperty);
+        return getPropertyById(newProperty.getId());
+    }
+
+    public PropertyDTO updateProperty(@NonNull UUID id, @NonNull PropertyDTO newProperty) {
+        Optional<Property> property = propertyRepository.findById(id);
+        if (property.isPresent()) {
+            property.get().setName(newProperty.getName());
+            property.get().setDescription(newProperty.getDescription());
+            property.get().setBathrooms(newProperty.getBathrooms());
+            property.get().setRooms(newProperty.getRooms());
+            property.get().setPricePerNight(newProperty.getPricePerNight());
+            property.get().setBbq(newProperty.isBbq());
+            property.get().setPetFriendly(newProperty.isPetFriendly());
+            property.get().setPool(newProperty.isPool());
+            propertyRepository.save(property.get());
+            return getPropertyById(property.get().getId());
+        } else
+            return null;
+    }
+
+    public Boolean deleteProperty(@NonNull UUID id) {
+        Optional<Property> property = propertyRepository.findById(id);
+        if (property.isPresent()) {
+            userService.removeProperty(property.get().getOwner().getId(), property.get());
+            propertyRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/web/mates/arriendatufinca/service/UserService.java
+++ src/main/java/web/mates/arriendatufinca/service/UserService.java
@@ -1,71 +1,85 @@
 package web.mates.arriendatufinca.service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.context.event.SpringApplicationEvent;
 import org.springframework.stereotype.Service;
 
 import lombok.NonNull;
 import web.mates.arriendatufinca.dto.RequestUserDTO;
 import web.mates.arriendatufinca.dto.UserDTO;
+import web.mates.arriendatufinca.model.Property;
 import web.mates.arriendatufinca.model.User;
 import web.mates.arriendatufinca.repository.UserRepository;
 
 @Service
 public class UserService {
 
-  private final UserRepository userRepository;
-  private final ModelMapper modelMapper;
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
 
-  UserService(UserRepository userRepository, ModelMapper modelMapper) {
-    this.userRepository = userRepository;
-    this.modelMapper = modelMapper;
-  }
-
-  public List<UserDTO> getAllUsers() {
-    Iterable<User> users = userRepository.findAll();
-    List<UserDTO> usersDTO = new ArrayList<>();
-
-    for (User user : users) {
-      usersDTO.add(modelMapper.map(user, UserDTO.class));
-    }
-    return usersDTO;
-  }
-
-  public UserDTO getUserById(@NonNull UUID id) {
-    Optional<User> user = userRepository.findById(id);
-    if (user.isPresent())
-      return modelMapper.map(user.get(), UserDTO.class);
-    return null;
-  }
-
-  public UserDTO newUser(@NonNull RequestUserDTO user) {
-    User newUser = modelMapper.map(user, User.class);
-
-    if (newUser == null) {
-      return null;
+    UserService(UserRepository userRepository, ModelMapper modelMapper) {
+        this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
     }
 
-    userRepository.save(newUser);
-    return getUserById(newUser.getId());
-  }
+    public List<UserDTO> getAllUsers() {
+        Iterable<User> users = userRepository.findAll();
+        List<UserDTO> usersDTO = new ArrayList<>();
 
-  public UserDTO updateUser(@NonNull UUID id, @NonNull UserDTO newUser) {
-    User user = userRepository.findById(id).get();
-    user.setName(newUser.getName());
-    user.setEmail(newUser.getEmail());
-    userRepository.save(user);
-    return newUser;
-  }
-
-  public Boolean deleteUser(@NonNull UUID id) {
-    if (userRepository.existsById(id)) {
-      userRepository.deleteById(id);
-      return true;
+        for (User user : users) {
+            usersDTO.add(modelMapper.map(user, UserDTO.class));
+        }
+        return usersDTO;
     }
-    return false;
-  }
+
+    public UserDTO getUserById(@NonNull UUID id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent())
+            return modelMapper.map(user.get(), UserDTO.class);
+        return null;
+    }
+
+    public UserDTO newUser(@NonNull RequestUserDTO user) {
+        User newUser = modelMapper.map(user, User.class);
+
+        if (newUser == null) {
+            return null;
+        }
+
+        userRepository.save(newUser);
+        return getUserById(newUser.getId());
+    }
+
+    public UserDTO updateUser(@NonNull UUID id, @NonNull UserDTO newUser) {
+        User user = userRepository.findById(id).get();
+        user.setName(newUser.getName());
+        user.setEmail(newUser.getEmail());
+        userRepository.save(user);
+        return newUser;
+    }
+
+    public void removeProperty(@NonNull UUID userId, @NonNull Property property) {
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isPresent()) {
+            Set<Property> properties = user.get().getProperties();
+            properties.remove(property);
+            user.get().setProperties(properties);
+            userRepository.save(user.get());
+        }
+    }
+
+    public Boolean deleteUser(@NonNull UUID id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            Set<Property> properties = user.get().getProperties();
+            properties.forEach(p -> p.setDeleted(true));
+            user.get().setProperties(properties);
+            userRepository.save(user.get());
+            userRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
