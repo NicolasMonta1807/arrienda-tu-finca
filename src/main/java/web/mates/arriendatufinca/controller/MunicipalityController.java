a4db0ae44ee40b292a6a5d611bbf4dbfa91d8735
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-04-03 20:47:34 -0500

    test: municipality controller

--- src/main/java/web/mates/arriendatufinca/controller/MunicipalityController.java
+++ src/main/java/web/mates/arriendatufinca/controller/MunicipalityController.java
@@ -23,33 +23,37 @@ public class MunicipalityController {
     }
 
     @GetMapping(value = {"", "/"})
-    public List<MunicipalityDTO> getAllMunicipalities() {
-        return municipalityService.getAll();
+    public ResponseEntity<List<MunicipalityDTO>> getAllMunicipalities() {
+        return new ResponseEntity<>(municipalityService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public MunicipalityDTO getMunicipalityById(@NonNull @PathVariable UUID id) {
+    public ResponseEntity<MunicipalityDTO> getMunicipalityById(@NonNull @PathVariable UUID id) {
         MunicipalityDTO municipality = municipalityService.getById(id);
-        if (municipality != null) {
-            return municipality;
+        if (municipality == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "municipality not found");
         }
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "municipality not found");
+        return new ResponseEntity<>(municipality, HttpStatus.OK);
     }
 
     @PostMapping(value = {"", "/"})
-    public MunicipalityDTO createMunicipality(@NonNull @Valid @RequestBody MunicipalityDTO municipalityDTO) {
-        return municipalityService.create(municipalityDTO);
+    public ResponseEntity<MunicipalityDTO> createMunicipality(@NonNull @Valid @RequestBody MunicipalityDTO municipalityDTO) {
+        return new ResponseEntity<>(municipalityService.create(municipalityDTO), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public MunicipalityDTO updateMunicipality(@NonNull @Valid @PathVariable UUID id, @NonNull @RequestBody MunicipalityDTO municipalityDTO) {
-        return municipalityService.update(id, municipalityDTO);
+    public ResponseEntity<MunicipalityDTO> updateMunicipality(@NonNull @Valid @PathVariable UUID id, @NonNull @RequestBody MunicipalityDTO municipalityDTO) {
+        MunicipalityDTO updatedMunicipality = municipalityService.update(id, municipalityDTO);
+        if (updatedMunicipality == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "municipality not found");
+        }
+        return new ResponseEntity<>(updatedMunicipality, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteMunicipality(@NonNull @PathVariable UUID id) {
+    public ResponseEntity<Void> deleteMunicipality(@NonNull @PathVariable UUID id) {
         municipalityService.delete(id);
-        return ResponseEntity.ok("");
+        return ResponseEntity.noContent().build();
     }
 
 }
