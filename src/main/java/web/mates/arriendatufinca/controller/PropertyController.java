6b87a7c70d344c4c2766ec1d6f9e9df2c36d4101
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-04-15 21:06:26 -0500

    fix: cors

--- src/main/java/web/mates/arriendatufinca/controller/PropertyController.java
+++ src/main/java/web/mates/arriendatufinca/controller/PropertyController.java
@@ -2,7 +2,7 @@ package web.mates.arriendatufinca.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.NonNull;
 import web.mates.arriendatufinca.dto.PropertyDTO;
@@ -12,15 +12,9 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/property")
 public class PropertyController {
 
--- src/main/java/web/mates/arriendatufinca/controller/UserController.java
+++ src/main/java/web/mates/arriendatufinca/controller/UserController.java
@@ -2,8 +2,7 @@ package web.mates.arriendatufinca.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.NonNull;
 import web.mates.arriendatufinca.dto.RequestUserDTO;
@@ -15,14 +14,9 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/user")
 public class UserController {
 
