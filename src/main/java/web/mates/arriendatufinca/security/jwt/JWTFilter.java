0f902043ebfde878aa421c172623e2b066949ba9
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-05-25 17:25:22 -0500

    fix: filter for api-docs

--- src/main/java/web/mates/arriendatufinca/security/jwt/JWTFilter.java
+++ src/main/java/web/mates/arriendatufinca/security/jwt/JWTFilter.java
@@ -34,7 +34,7 @@ public class JWTFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
-        if (request.getServletPath().matches("/auth/login|/auth/signup|/auth/verify"))
+        if (request.getServletPath().matches("/auth/login|/auth/signup|/auth/verify|^/api-docs/swagger-(ui|config)/[^/]+$"))
             filterChain.doFilter(request, response);
         else {
             String authorizationHeader = request.getHeader("Authorization");
