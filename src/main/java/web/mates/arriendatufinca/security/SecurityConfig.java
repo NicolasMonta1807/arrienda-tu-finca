2ccfd77ed168515acded5804f447267c70dc605c
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-05-25 12:35:27 -0500

    fix: cors configuration

--- src/main/java/web/mates/arriendatufinca/security/SecurityConfig.java
+++ src/main/java/web/mates/arriendatufinca/security/SecurityConfig.java
@@ -2,9 +2,7 @@ package web.mates.arriendatufinca.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,7 +13,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import web.mates.arriendatufinca.security.jwt.JWTFilter;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -33,12 +35,22 @@ public class SecurityConfig {
     }
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.applyPermitDefaultValues();
+        configuration.addAllowedOriginPattern("*");
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(request -> new CorsConfiguration()
-                        .applyPermitDefaultValues()
-                        .addAllowedMethod("OPTIONS"))
+                .cors(c -> corsConfigurationSource())
                 .authorizeHttpRequests(request -> {
                     // Auth requests
                     request.requestMatchers("/auth/signup").permitAll();
@@ -58,7 +70,7 @@ public class SecurityConfig {
                             "/webjars/**")
                             .permitAll();
 
-                    // Any request
+                    // Any other request
                     request.anyRequest().authenticated();
                 })
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
